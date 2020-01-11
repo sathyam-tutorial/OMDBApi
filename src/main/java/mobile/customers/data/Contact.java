29b59b03a248428403acf45f01fd58eebf5374cc
sathyam-tutorial <techsathya77@gmail.com>
2020-01-11 15:56:41 -0600

    Updating mobile application with more options

--- src/main/java/mobile/customers/data/Contact.java
+++ src/main/java/mobile/customers/data/Contact.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class Contact {
-
     private String name;
     private String phoneNumber;
     private ArrayList<String> phoneContact = new ArrayList<String>();
@@ -14,14 +13,61 @@ public class Contact {
     private int numberIndex;
     private List<Integer> queryContactList;
 
+    //Adding to PhoneContact List
+
+    /*public Contact() {
+       phoneContact.add("Uma");
+       phoneContact.add("23243");
+       phoneContact.add("Nagaraju");
+       phoneContact.add("2121");
+       phoneContact.add("Salatha");
+       phoneContact.add("5454");
+    } */
+    public List<String> manageContact(String name, String purpose) {
+        if(purpose == "query") {
+            List<String> queryContact = queryContact(name);
+            return queryContact;
+        }
+        if(purpose == "delete") {
+            List<String> deleteContact = deleteContact(name);
+            return deleteContact;
+        }
+        return null;
+    }
+
     public List<String> storeContact(String name, String phoneNumber) {
         phoneContact.add(name);
         phoneContact.add(phoneNumber);
         return Arrays.asList(name, phoneNumber);
     }
-    public ArrayList<String> showContacts() {
-        return phoneContact;
+
+    private List<Integer> getIndex(String name) {
+        int index = phoneContact.indexOf(name);
+        if(index == -1) {
+            return Collections.singletonList(null);
+        }
+        int nameIndex = index;
+        int numberIndex = (index + 1);
+        return Arrays.asList(nameIndex, numberIndex);
     }
+
+    public List<String> validate(String name) {
+        List<String> myValidationList = queryContact(name);
+        return (myValidationList);
+    }
+
+    private List<String> queryContact(String name) {
+        queryContactList = getIndex(name);
+        if(queryContactList.contains(null)) {
+            return Collections.singletonList(null);
+        }
+        System.out.println("Name Index query is " + queryContactList.get(0));
+        System.out.println("Number Index query is " + queryContactList.get(1));
+        String contactName = phoneContact.get(queryContactList.get(0));
+        String contactNumber = phoneContact.get(queryContactList.get(1));
+        return Arrays.asList(contactName, contactNumber);
+    }
+
     public List<String> modifyContact(String name, String newName, String newPhoneNumber) {
         List<String> modifyContact = queryContact(name);
         if(modifyContact.contains(null)) {
@@ -34,27 +80,9 @@ public class Contact {
         List<String> newModifyContact = queryContact(newName);
         return (newModifyContact);
     }
-    public List<String> manageContact(String name, String purpose) {
-        if(purpose == "query") {
-            List<String> queryContact = queryContact(name);
-            return queryContact;
-        }
-        if(purpose == "delete") {
-            List<String> deleteContact = deleteContact(name);
-            return deleteContact;
-        }
-        return null;
-    }
-    private List<String> queryContact(String name) {
-        queryContactList = getIndex(name);
-        if(queryContactList.contains(null)) {
-            return Collections.singletonList(null);
-        }
-        System.out.println("Name Index query is " + queryContactList.get(0));
-        System.out.println("Number Index query is " + queryContactList.get(1));
-        String contactName = phoneContact.get(queryContactList.get(0));
-        String contactNumber = phoneContact.get(queryContactList.get(1));
-        return Arrays.asList(contactName, contactNumber);
+
+    public ArrayList<String> showContacts() {
+        return phoneContact;
     }
     private List<String> deleteContact (String name) {
         List<String> nameList = queryContact(name);
@@ -66,14 +94,4 @@ public class Contact {
         System.out.println("phoneContact is " + phoneContact);
         return (nameList);
     }
-    private List<Integer> getIndex(String name) {
-        int index = phoneContact.indexOf(name);
-        if(index == -1) {
-            return Collections.singletonList(null);
-        }
-        int nameIndex = index;
-        int numberIndex = (index + 1);
-        return Arrays.asList(nameIndex, numberIndex);
-    }
-
 }
--- src/main/java/mobile/customers/data/mobileProject.java
+++ src/main/java/mobile/customers/data/mobileProject.java
@@ -7,6 +7,12 @@ import java.util.Scanner;
 
 public class mobileProject {
     private static final String LIST_CONTACT = "# To Get List of All Contacts Type 1 \n";
+    private static final String ADD_CONTACT = "# To Add a Contact Type 2 \n";
+    private static final String MODIFY_CONTACT = "# To Change a existing contact Type 3 \n";
+    private static final String DELETE_CONTACT = "# To Delete a contact Type 4 \n";
+    private static final String FIND_CONTACT = "# To Find a contact Type 5 \n";
+    private static final String EXIT = "# To Quit Type 6 \n";
+    private static final String HELP = "# To Print Instructions Type 7 \n";
     private static final String ENTER_CHOICE = "Please Enter the Choice: ";
     private static final String SHOW_HELP = "For Instructions Type 7 \r";
     private static final String INPUT_EXCEPTION = "Invalid Input, " +
@@ -14,6 +20,7 @@ public class mobileProject {
             "For help please type 7 \n";
     private static Scanner scanner = new Scanner(System.in);
     private static Contact contact = new Contact();
+    private static java.lang.String String;
 
     public static void main(String[] args) {
         boolean quit = false;
@@ -29,21 +36,46 @@ public class mobileProject {
                     listAllContacts();
                     break;
                 case 2:
-                    addContact();
+                    addContact(String, String);
                     break;
                 case 3:
-                    modifyContact();
+                    modifyContact(String, String, String);
                     break;
                 case 4:
-                    deleteContact();
+                    deleteContact(String);
+                    break;
+                case 5:
+                    findContact(String);
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    printInstructions();
+                    break;
+                case 8:
+                    validate();
                     break;
                 default:
                     System.out.println(INPUT_EXCEPTION);
             }
         }
     }
+    private static void printInstructions() {
+        System.out.print(LIST_CONTACT);
+        System.out.print(ADD_CONTACT);
+        System.out.print(MODIFY_CONTACT);
+        System.out.print(DELETE_CONTACT);
+        System.out.print(FIND_CONTACT);
+        System.out.print(EXIT);
+        System.out.print(HELP);
+    }
+    private static void validate() {
+        String name = scanner.next();
+        List<String> validateList = contact.validate(name);
+        System.out.println("Validate List is " + validateList);
 
-    private static void listAllContacts() {
+    }
+    private static String listAllContacts() {
         ArrayList<String> myContactList = contact.showContacts();
         if(myContactList != null && !myContactList.isEmpty()) {
             System.out.println("List of Contacts");
@@ -54,36 +86,38 @@ public class mobileProject {
             System.out.println("No Contact List found, \n" +
                     "Please add new contact list");
         }
+        return String;
     }
-    private static void addContact() {
+    public static String addContact(String name, String number) {
         System.out.println("Please Enter Contact Name to add");
-        String name = scanner.next();
+        name = scanner.next();
         System.out.println("Please Enter Phone Number to add");
-        String number = scanner.next();
+        number = scanner.next();
         contact.storeContact(name, number);
         System.out.println("Successfully Added " + name + " and " + number + " to Mobile");
+        return String;
     }
 
-    private static void modifyContact() {
+    private static String modifyContact(String name, String newName, String newNumber ) {
         System.out.println("Please Enter Existing Contact Name to change");
-        String name = scanner.next();
+        name = scanner.next();
         System.out.println("Please Enter New Contact Name to change");
-        String newName = scanner.next();
+        newName = scanner.next();
         System.out.println("Please Enter New Phone Number to change");
-        String newNumber = scanner.next();
+        newNumber = scanner.next();
         List<String> modifyContactList = contact.modifyContact(name,newName,newNumber);
         if(modifyContactList.contains(null)) {
             System.out.println("Name not found in Mobile," +
                     "\nPlease re-check the name and Type again");
-            return;
         }
         System.out.println("Contact has been updated with name " + newName +
                 " and Number " + newNumber + "\nUpdated Details are " + modifyContactList);
+        return String;
     }
-    private static void deleteContact() {
+    private static String deleteContact(String name) {
         String purpose = "delete";
         System.out.println("Please Enter the name of the Contact to Delete:");
-        String name = scanner.next();
+        name = scanner.next();
         List<String> deleteContactList = contact.manageContact(name, purpose);
         if(deleteContactList.contains(null)) {
             System.out.println("Contact name " + name + " not found");
@@ -91,5 +125,19 @@ public class mobileProject {
         else {
             System.out.println(deleteContactList + " Contact Deleted");
         }
+        return String;
+    }
+    public static String findContact(String name) {
+        String purpose = "query";
+        System.out.println("Please Enter the Contact name to LookUp: ");
+        name = scanner.next();
+        List<String> getContact = contact.manageContact(name, purpose);
+        if(getContact.contains(null)) {
+            System.out.println("Contact name " + name + " not found");
+        }
+        else {
+            System.out.println("Contact Name Found" + getContact);
+        }
+        return String;
     }
 }
